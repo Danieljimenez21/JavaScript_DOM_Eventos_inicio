70bdf393e14117e8bda802395d5d74cc22e77742
Daniel_Jimenez <djimeneza7b2276@gmail.com>
2025-03-11 10:53:00 -0600

    Crear HTML con querySelectorAll

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,24 @@ enlaces[0].textContent = 'Nuevo texto para el enlace';
 enlaces[0].classList.add('nueva-clase');
 // enlaces[0].classList.remove('navegacion__enlace');
 
-
 //getElementById
 // const heading2 = getElementById('heading');
 // console.log(heading2);
+//generar un nuevo en lace 
+const nuevoEnlace = document.createElement('A') //solo etiquetas en mayusculas
+
+//agregar href
+nuevoEnlace.href = 'nuevo-enlace.html';
+
+//agregar el texto
+nuevoEnlace.textContent = 'Tienda virtual';
+
+//agregar la clase
+nuevoEnlace.classList.add('navegacion__enlace');
+
+//agragarlo al documento
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace);
+
+
+console.log(nuevoEnlace)
